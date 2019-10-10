cae07e72bce4f7dd487fe51d0bca4c5bc80ea711
Alireza Oroumand <oroumand@outlook.com>
2019-10-10 19:11:18 +0330

    افزودن لایه API به پروژه

---
+++ 02. Infrastructures/Data/Bazzar.Infrastructures.Data.InMemory/FakeUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Framework.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bazzar.Infrastructures.Data.InMemory
+{
+    public class FakeUnitOfWork : IUnitOfWork
+    {
+        public int Commit()
+        {
+            return 0;
+        }
+    }
+}
---
+++ 03. EndPoints/Bazzar.EndPoints.API/Controllers/AddvertismentController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bazzar.Core.ApplicationServices.Advertisements.CommandHandlers;
+using Bazzar.Core.Domain.Advertisements.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bazzar.EndPoints.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddvertismentController : ControllerBase
+    {
+
+
+        [HttpPost]
+        public IActionResult Post([FromServices] CreateHandler handler, Create request)
+        {
+            handler.Handle(request);
+            return Ok();
+        }
+
+        [Route("title")]
+        [HttpPut]
+        public IActionResult Put([FromServices] SetTitleHandler handler, SetTitle request)
+        {
+            handler.Handle(request);
+            return Ok();
+        }
+
+        [Route("text")]
+        [HttpPut]
+        public IActionResult Put([FromServices] UpdateTextHandler handler, UpdateText request)
+        {
+            handler.Handle(request);
+            return Ok();
+        }
+
+        [Route("price")]
+        [HttpPut]
+        public IActionResult Put([FromServices] UpdatePriceHandler handler, UpdatePrice request)
+        {
+            handler.Handle(request);
+            return Ok();
+        }
+
+        [Route("publish")]
+        [HttpPut]
+        public IActionResult Put([FromServices] RequestToPublishHandler handler, RequestToPublish request)
+        {
+            handler.Handle(request);
+            return Ok();
+        }
+    }
+}
