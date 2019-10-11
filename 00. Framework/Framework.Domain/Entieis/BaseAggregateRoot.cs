7a2b1f09a83baeb6379936f0a0ca83dfd1f48c77
Alireza Oroumand <oroumand@outlook.com>
2019-10-11 09:41:57 +0330

    افزودن == به BaseAggregateRoot

--- 00. Framework/Framework.Domain/Entieis/BaseAggregateRoot.cs
+++ 00. Framework/Framework.Domain/Entieis/BaseAggregateRoot.cs
@@ -1,11 +1,12 @@
 ﻿using Framework.Domain.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Framework.Domain.Entieis
 {
 
-    public abstract class BaseAggregateRoot<TId>
+    public abstract class BaseAggregateRoot<TId> where TId:IEquatable<TId>
     {
 
         private readonly List<IEvent> _events;
@@ -21,5 +22,46 @@ namespace Framework.Domain.Entieis
         public IEnumerable<object> GetEvents() => _events.AsEnumerable();
         public void ClearEvents() => _events.Clear();
         protected abstract void ValidateInvariants();
+
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BaseAggregateRoot<TId>;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == default || other.Id == default)
+                return false;
+
+            return Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(BaseAggregateRoot<TId> a, BaseAggregateRoot<TId> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(BaseAggregateRoot<TId> a, BaseAggregateRoot<TId> b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
     }
 }
