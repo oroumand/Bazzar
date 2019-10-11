5930b6aa43baf7f7c2afd2b2adc592981cc6954a
Alireza Oroumand <oroumand@outlook.com>
2019-10-11 09:39:48 +0330

    اضافه کردن == به BaseEntity

--- 00. Framework/Framework.Domain/Entieis/BaseEntity.cs
+++ 00. Framework/Framework.Domain/Entieis/BaseEntity.cs
@@ -22,7 +22,7 @@ namespace Framework.Domain.Entieis
     //    protected abstract void ValidateInvariants();
     //    protected abstract void SetStateByEvent(IEvent @event);
     //}
-    public abstract class BaseEntity<TId>
+    public abstract class BaseEntity<TId> where TId :IEquatable<TId>
     {
         public TId Id { get; protected set; }
         private Action<IEvent> _applier;
@@ -39,6 +39,46 @@ namespace Framework.Domain.Entieis
         }
 
         protected abstract void SetStateByEvent(IEvent @event);
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BaseEntity<TId>;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == default || other.Id == default)
+                return false;
+
+            return Id.Equals(other.Id);
+        }
+
+        public static bool operator ==(BaseEntity<TId> a, BaseEntity<TId> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(BaseEntity<TId> a, BaseEntity<TId> b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
 
 
     }
